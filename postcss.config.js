39d40efb286d6acfcbe32cc38c49806ed61693bc
devferx <ferkki@hotmail.com>
2021-05-30 12:08:15 -0400

    :card_file_box: [UPDATE] simplified postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,6 @@
-require('dotenv').config();
-
-const isProd = process.env.ENV === 'production';
-
-const plugins = [
-    require('tailwindcss'),
-    require('autoprefixer'),
-]
-
-isProd && plugins.push(require('cssnano'));
-
 module.exports = {
-  plugins
-}
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
