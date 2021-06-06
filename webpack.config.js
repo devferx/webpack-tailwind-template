e8fc91eb318d2ef06f02638d5fa7a7494b6c77ca
devferx <ferkki@hotmail.com>
2021-06-06 19:38:52 -0400

    :arrow_up: [UPDATE] Dev Dependencies and Tailwind JIT

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,45 @@
-require('dotenv').config();
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin  =  require('html-webpack-plugin');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const isProd = process.env.ENV === 'production';
+const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src/index.js'),
+  entry: path.resolve(__dirname, "src/index.js"),
   mode: process.env.ENV,
   output: {
     filename: "[name].js",
-    path: path.resolve('./dist'),
-    publicPath: '/',
+    path: path.resolve("./dist"),
+    publicPath: "/",
   },
   devServer: {
     port: 5050,
-    contentBase: path.join(__dirname, 'dist')
+    contentBase: path.join(__dirname, "dist"),
   },
   module: {
     rules: [
       {
         test: /\.html$/,
-        use: ['html-loader']
+        use: ["html-loader"],
       },
       {
         test: /\.css$/,
-        use: [ isProd ? MiniCssExtractPlugin.loader : "style-loader", "css-loader", "postcss-loader"]
-      }
-    ]
+        use: [
+          isProd ? MiniCssExtractPlugin.loader : "style-loader",
+          "css-loader",
+          "postcss-loader",
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'public/index.html')
+      template: path.resolve(__dirname, "public/index.html"),
     }),
-    isProd? new MiniCssExtractPlugin() : () => {}
-  ]
-}
+    isProd ? new MiniCssExtractPlugin() : () => {},
+  ],
+};
